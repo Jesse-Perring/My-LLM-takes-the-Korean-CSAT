10cb2648a727052900c64c6b02850a7f6fc314f1
forlyby <forlyby@naver.com>
2025-06-06 21:10:04 +0900

    결과파일추가

--- Run_llm_by_llm/GPT4o/run.py
+++ Run_llm_by_llm/GPT4o/run.py
@@ -35,7 +35,7 @@ import os
 # ===================================================
 
 MODEL_NAME = "GPT4o"
-PROMPTING_METHOD = "ToT"
+PROMPTING_METHOD = "PromptChaining"
 DATA_FILENAME = "202511_en.json"
 
 DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../data/json'))
